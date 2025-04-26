5d00c7ebba55789cddf50de6a1a855c94d37ba88
Diogo Lima <diogocerqueiradeelima@gmail.com>
2025-04-26 19:07:35 +0100

    fix: protection of resources in BFF

--- gateway/src/main/java/com/github/diogocerqueiralima/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/github/diogocerqueiralima/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.github.diogocerqueiralima.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
@@ -19,6 +20,7 @@ public class GatewayApplication {
                 .route("forum-service",
                         r -> r
                                 .path("/api/v1/topics/**", "/api/v1/comments/**", "/v3/api-docs/**", "/api/v1/forum/swagger-ui/**", "api/v1/forum/docs.html")
+                                .filters(GatewayFilterSpec::tokenRelay)
                                 .uri("http://forum-service:8080")
                 )
                 .build();

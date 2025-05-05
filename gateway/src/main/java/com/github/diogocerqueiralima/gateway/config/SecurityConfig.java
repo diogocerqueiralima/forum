ca9ddc71f16c3e79bb6a1a90e404d8a17a39d421
Diogo Lima <diogocerqueiradeelima@gmail.com>
2025-05-05 09:52:19 +0100

    feat: integrate front-end into oauth flow

--- gateway/src/main/java/com/github/diogocerqueiralima/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/github/diogocerqueiralima/gateway/config/SecurityConfig.java
@@ -3,10 +3,20 @@ package com.github.diogocerqueiralima.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,14 +25,39 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain defaultSecurityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange ->
                         exchange
                                 .pathMatchers(HttpMethod.GET, "/info/me").authenticated()
                                 .anyExchange().permitAll()
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .exceptionHandling(exchange -> exchange
+                        .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .oauth2Login(customizer -> customizer
+                        .authenticationSuccessHandler((webFilterExchange, _) -> {
+                            webFilterExchange.getExchange().getResponse().setStatusCode(HttpStatus.PERMANENT_REDIRECT);
+                            webFilterExchange.getExchange().getResponse().getHeaders().add("Location", "http://localhost:3000");
+                            return Mono.empty();
+                        })
+                )
                 .build();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
 }
--- gateway/src/main/java/com/github/diogocerqueiralima/gateway/controllers/InfoController.java
+++ gateway/src/main/java/com/github/diogocerqueiralima/gateway/controllers/InfoController.java
@@ -1,24 +1,24 @@
 package com.github.diogocerqueiralima.gateway.controllers;
 
 import com.github.diogocerqueiralima.gateway.dto.UserDto;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/info")
 public class InfoController {
 
     @GetMapping("/me")
-    public ResponseEntity<UserDto> me(@AuthenticationPrincipal OidcUser user) {
+    public Mono<UserDto> me(@AuthenticationPrincipal OidcUser user) {
 
         OidcUserInfo userInfo = user.getUserInfo();
 
-        return ResponseEntity.ok(
+        return Mono.just(
                 new UserDto(
                         userInfo.getEmail(), userInfo.getGivenName(), userInfo.getFamilyName(),
                         userInfo.getFullName(), userInfo.getPicture()
